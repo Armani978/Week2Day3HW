f22dcd058aa502410b6a9b8c82ca6f01a7628498
Armani978 <100504237+Armani978@users.noreply.github.com>
2022-04-14 01:43:48 -0700

    first commit

---
+++ area.py
@@ -0,0 +1,10 @@
+
+
+def areafunc():
+    area = int(input("Enter length: ")) * int(input("Enter width: "))
+    print(area)
+    
+    
+
+
+areafunc()
---
+++ circle.py
@@ -0,0 +1,9 @@
+from cmath import pi
+import math
+
+def circle():
+    radius = float(input("Enter radius: "))
+    c = 2 * pi * radius
+    print(c)
+
+circle()
